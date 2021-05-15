083edd761b4521aae1f0aa71283a623106db6cfb
rom <47617159+leedokchidok19@users.noreply.github.com>
2021-05-15 20:39:14 +0900

    pom.xml maven 버전 변경 - 3.1.1.RELEASE -> 5.0.7.RELEASE
    maven-compiler-plugin
    - 2.5.1 -> 3.5.1
    
    Lombok
    - lombok 1.18.12 추가

--- boilerplate-spring-xml/src/main/java/io/leedokchidok/boilerplate/HomeController.java
+++ boilerplate-spring-xml/src/main/java/io/leedokchidok/boilerplate/HomeController.java
@@ -11,6 +11,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import lombok.Data;
+
 /**
  * Handles requests for the application home page.
  */
@@ -35,5 +37,5 @@ public class HomeController {
 		
 		return "home";
 	}
-	
+
 }
---
+++ boilerplate-spring-xml/src/main/java/io/leedokchidok/boilerplate/common/service/CommonVO.java
@@ -0,0 +1,8 @@
+package io.leedokchidok.boilerplate.common.service;
+
+import lombok.Data;
+
+@Data
+public class CommonVO {
+
+}
