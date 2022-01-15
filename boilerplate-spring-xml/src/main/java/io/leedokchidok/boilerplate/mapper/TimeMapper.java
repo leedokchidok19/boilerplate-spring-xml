5f4e0ad4e0b56b2db5763ec556d9f124444d60f6
rom <47617159+leedokchidok19@users.noreply.github.com>
2022-01-16 01:32:10 +0900

    test:주석 추가, 불필요한 @Repository 제거

--- boilerplate-spring-xml/src/main/java/io/leedokchidok/boilerplate/mapper/TimeMapper.java
+++ boilerplate-spring-xml/src/main/java/io/leedokchidok/boilerplate/mapper/TimeMapper.java
@@ -1,9 +1,7 @@
 package io.leedokchidok.boilerplate.mapper;
 
 import org.apache.ibatis.annotations.Select;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface TimeMapper {
 
 	//Oracle
