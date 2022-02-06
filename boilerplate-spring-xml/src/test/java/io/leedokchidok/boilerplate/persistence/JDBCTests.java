c95e831e26a79f8b70ee8b512f987f3d4819a54d
rom <47617159+leedokchidok19@users.noreply.github.com>
2022-02-06 22:37:44 +0900

    test:JDBCTests 계정 정보 변수로 분리

--- boilerplate-spring-xml/src/test/java/io/leedokchidok/boilerplate/persistence/JDBCTests.java
+++ boilerplate-spring-xml/src/test/java/io/leedokchidok/boilerplate/persistence/JDBCTests.java
@@ -22,11 +22,17 @@ public class JDBCTests {
 
 	@Test
 	public void testConnection() {
+
+		//DB schema
+		String schema = "DB_Schema";
+		String username = "ex_username";
+		String password = "ex_password";
+
 		try (Connection con =
-				DriverManager.getConnection(//mysql 연결
-						"jdbc:log4jdbc:mysql://127.0.0.1:3306/DB_Schema?serverTimezone=UTC&useSSL=false",
-						"ex_username",
-						"ex_password")){
+				DriverManager.getConnection(//mysql 연결 : DB_Schema/ex_username/ex_password
+						"jdbc:log4jdbc:mysql://127.0.0.1:3306/"+schema+"?serverTimezone=UTC&useSSL=false",
+						username,
+						password)){
 			log.info(con);
 		} catch (Exception e) {
 			fail(e.getMessage());
